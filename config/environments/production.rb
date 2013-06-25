8290c0bab48dd49382a53ae2191fdf140981ff63
Kevin Lester <kevin@e-kevin.com>
2013-06-25 15:44:01 -0400

    latest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,9 @@ Prayerlist::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #config.assets.compile = false
+  # WKL: Need to set this up so css is compiled at runtime
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
@@ -66,7 +68,7 @@ Prayerlist::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   # To build url in emails
-  config.action_mailer.default_url_options = { :host => 'prayer.e-kevin.com:3000' }
+  config.action_mailer.default_url_options = { :host => 'prayer.e-kevin.com' }
 
   # smtp configuration
   config.action_mailer.delivery_method = :smtp
